77718b4f77e16342e6d055fd877c06ae6febe3bb
rOmAn2193 <roman_puzey@aol.com>
2015-11-02 13:51:07 -0700

    Finished The Pokedex App

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Roman on 11/1/15.
+//  Copyright © 2015 Roman Puzey. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -12,9 +12,8 @@ class PokemonDetailVC: UIViewController {
     @IBOutlet weak var currentEvoImg: UIImageView!
     @IBOutlet weak var nextEvoImg: UIImageView!
     @IBOutlet weak var evoLbl: UILabel!
-
-
-
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var baseAttack: UILabel!
 
     var pokemon: Pokemon!
 
@@ -22,7 +21,45 @@ class PokemonDetailVC: UIViewController {
         super.viewDidLoad()
 
         nameLbl.text = pokemon.name
+        let img = UIImage(named: "\(pokemon.pokedexId)")
+        mainImg.image = img
+        currentEvoImg.image = img
+
+        pokemon.downloadPokemonDetails { () -> () in
+            //this will be called after download is done
+            self.updateUI()
+        }
     }
+
+    func updateUI()
+    {
+        descriptionLbl.text = pokemon.description
+        typeLbl.text = pokemon.type
+        defenseLbl.text = pokemon.defense
+        heightLbl.text = pokemon.height
+        pokedexLbl.text = "\(pokemon.pokedexId)"
+        weightLbl.text = pokemon.weight
+        baseAttack.text = pokemon.attack
+
+        if pokemon.nextEvolutionId == ""
+        {
+            evoLbl.text = "No Evolutions"
+            nextEvoImg.hidden = true
+        }
+        else
+        {
+            nextEvoImg.hidden = false
+            nextEvoImg.image = UIImage(named: pokemon.nextEvolutionId)
+            var str = "Next Evolution: \(pokemon.nextEvolutionTxt)"
+
+            if pokemon.nextEvolutionLvl != ""
+            {
+                str += " - LVL \(pokemon.nextEvolutionLvl) "
+            }
+        }
+    }
+
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
